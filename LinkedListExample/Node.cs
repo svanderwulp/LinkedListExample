62b18edcaadbb84066450a47076541c315f38bc9
Bas van der Wulp <bas.vanderwulp@fontys.nl>
2021-09-13 14:50:26 +0200

    Add project files.

---
+++ LinkedListExample/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int value, Node next)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+    }
+}
---
+++ LinkedListExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list1 = new LinkedList();
+            Console.WriteLine("Inhoud van de list: {0}", list1.ToString());
+            Console.WriteLine("Aantal items in list: {0}", list1.Length);
+            list1.Insert(12);
+            list1.Insert(14);
+            list1.Insert(45);
+            Console.WriteLine("Inhoud van de list: {0}", list1.ToString());
+            Console.WriteLine("Aantal items in list: {0}\n", list1.Length);
+            for(int i = 0; i < 4; i++)
+            {
+                var popped = list1.Delete();
+                Console.WriteLine("Verwijderd item: {0}", popped);
+                Console.WriteLine("Inhoud van de list: {0}", list1.ToString());
+                Console.WriteLine("Aantal items in list: {0}\n", list1.Length);
+            }
+            
+            Console.WriteLine("Druk Enter om door te gaan");
+            string input = Console.ReadLine();
+        }
+    }
+}
